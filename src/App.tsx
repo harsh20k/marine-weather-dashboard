0802b28d697f79176482ce45e33d950d3608bc3e
harsh20k <harsh.pandey77@gmail.com>
2025-10-09 10:05:19 -0300

    duplicate import removal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
+import { useEffect } from "react";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { useState, useEffect } from "react";
 import './App.css'
 
 function App() {
